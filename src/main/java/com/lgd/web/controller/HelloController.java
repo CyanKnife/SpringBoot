a0ac1f1aaab1d1c2e7e6fe99df283411d2f6f776
CyanKnife <wqndyxh@foxmail.com>
2017-07-18 18:53:28 +0800

    java 新特性测试

--- src/main/java/com/lgd/web/controller/HelloController.java
+++ src/main/java/com/lgd/web/controller/HelloController.java
@@ -1,7 +1,20 @@
 package com.lgd.web.controller;
 
+import org.codehaus.groovy.runtime.powerassert.SourceText;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Author : linguodong
@@ -16,4 +29,338 @@ public class HelloController {
     public String say() {
         return "Hello SpringBoot";
     }
+
+    /**
+     * java8 lambda
+     */
+
+    public static void testLambda() {
+        // Java 8之前：
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Before Java8, too much code for too little to do");
+            }
+        }).start();
+
+        //Java 8方式：
+        new Thread(() -> System.out.println("In Java8, Lambda expression rocks !!")).start();
+    }
+
+    public static void testLambda1() {
+        // Java 8之前：
+        JButton jButton = new JButton("Show");
+        jButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Event handling without lambda expression is boring");
+            }
+        });
+        // Java 8方式：
+        jButton.addActionListener((e) -> {
+            System.out.println("Light, Camera, Action !! Lambda expressions Rocks");
+        });
+    }
+
+    public static void testLambda2() {
+        // Java 8之前：
+        List<String> features = Arrays.asList("Lambdas", "Default Method", "Stream API", "Date and Time API");
+        for (String feature : features) {
+            System.out.println(feature);
+        }
+        // Java 8之后：
+        List<String> features2 = Arrays.asList("Lambdas", "Default Method", "Stream API", "Date and Time API");
+        features2.forEach(n -> System.out.println(n));
+        features2.forEach(System.out::println);
+        features2.forEach((String n) -> System.out.println("*" + n + "*"));
+        //features2.forEach(n -> {System.out.println(n);});
+        // 使用Java 8的方法引用更方便，方法引用由::双冒号操作符标示，
+        // 看起来像C++的作用域解析运算符
+        features2.forEach(System.out::println);
+        //features2.forEach(n -> System.out.println(n));
+    }
+
+    public static void filter(List<String> names, Predicate condition) {
+        for (String name : names) {
+            if (condition.test(name)) {
+                System.out.println(name + " ");
+            }
+        }
+    }
+
+    // 更好的办法
+    public static void filter2(List names, Predicate condition) {
+        names.stream().filter((name) -> (condition.test(name))).forEach((name) -> {
+            System.out.println(name + " ");
+        });
+    }
+
+    public static void testLambda3() {
+        List<String> languages = Arrays.asList("Java", "Scala", "C++", "Haskell", "Lisp", "Python", "Erlang", "Ruby", "Lua", "C#");
+        System.out.println("Languages which starts with J :");
+        filter(languages, (str) -> ((String) str).startsWith("J"));
+
+        System.out.println("Languages which ends with a ");
+        filter(languages, (str) -> ((String) str).endsWith("a"));
+
+        System.out.println("Print all languages :");
+        filter(languages, (str) -> true);
+
+        System.out.println("Print no language : ");
+        filter(languages, (str) -> false);
+
+        System.out.println("Print language whose length greater than 4:");
+        filter(languages, (str) -> ((String) str).length() > 4);
+    }
+
+    public static void testLambda4() {
+        List<String> names = Arrays.asList("Java", "Scala", "C++", "Haskell", "Lisp", "Python", "Erlang", "Ruby", "Lua", "C#");
+        // 甚至可以用and()、or()和xor()逻辑函数来合并Predicate，
+        // 例如要找到所有以J开始，长度为四个字母的名字，你可以合并两个Predicate并传入
+        Predicate<String> startsWithJ = (n) -> n.startsWith("J");
+        Predicate<String> fourLetterLong = (n) -> n.length() == 4;
+        names.stream().filter(startsWithJ.and(fourLetterLong)).forEach(
+                (n) -> System.out.print("nName, which starts with 'J' and four letter long is : " + n));
+    }
+
+    public static void testLambda5() {
+        // 不使用lambda表达式为每个订单加上12%的税
+        List<Integer> costBeforeTax = Arrays.asList(100, 200, 300, 400, 500);
+        for (Integer cost : costBeforeTax) {
+            double price = cost + .12 * cost;
+            System.out.println(price);
+        }
+        // 使用lambda表达式
+        List<Integer> costBeforeTax2 = Arrays.asList(100, 200, 300, 400, 500);
+        costBeforeTax2.stream().map((cost) -> cost + .12 * (Integer) cost).forEach(System.out::println);
+    }
+
+    public static void testLambda6() {
+        // 为每个订单加上12%的税
+        // 老方法：
+        List<Integer> costBeforeTax = Arrays.asList(100, 200, 300, 400, 500);
+        double total = 0;
+        for (Integer cost : costBeforeTax) {
+            double price = cost + .12 * cost;
+            total = total + price;
+        }
+        System.out.println("Total : " + total);
+        List<Integer> costBeforeTax2 = Arrays.asList(100, 200, 300, 400, 500);
+        //double bill = costBeforeTax2.stream().map((cost) -> cost + .12 * (Integer) cost).reduce((sum, cost) -> sum + cost).get();
+        double bill = costBeforeTax2.stream().mapToInt((cost) -> (int) (cost + .12 * (Integer) cost)).sum();
+        System.out.println("Total : " + bill);
+    }
+
+    public static void testLambda7() {
+        // 创建一个字符串列表，每个字符串长度大于2
+        List<String> strList = Arrays.asList("abc", "bcd", "", "defg", "jk");
+        List<String> filtered = strList.stream().filter(x -> ((String) x).length() > 2).collect(Collectors.toList());
+        System.out.printf("Original List : %s, filtered list : %s %n", strList, filtered);
+    }
+
+    public static void testLambda8() {
+        // 将字符串换成大写并用逗号链接起来
+        List<String> G7 = Arrays.asList("USA", "Japan", "France", "Germany", "Italy", "U.K.", "Canada");
+
+        String G7Countries = G7.stream().map(x -> x.toUpperCase()).collect(Collectors.joining(","));
+        System.out.println(G7Countries);
+        System.out.println(G7.stream().collect(Collectors.joining(",")));
+    }
+
+    public static void testLambda9() {
+        // 用所有不同的数字创建一个正方形列表
+        List<Integer> numbers = Arrays.asList(9, 10, 3, 4, 7, 3, 4);
+        List<Integer> distinct = numbers.stream().map(i -> i * i).distinct().collect(Collectors.toList());
+        System.out.printf("Original List : %s,  Square Without duplicates : %s %n", numbers, distinct);
+    }
+
+    public static void testLambda10() {
+        //获取数字的个数、最小值、最大值、总和以及平均值
+        List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+        IntSummaryStatistics stats = primes.stream().mapToInt((x) -> x).summaryStatistics();
+        System.out.println("Highest prime number in List : " + stats.getMax());
+        System.out.println("Lowest prime number in List : " + stats.getMin());
+        System.out.println("Sum of all prime numbers : " + stats.getSum());
+        System.out.println("Average of all prime numbers : " + stats.getAverage());
+    }
+
+
+    /**
+     * java8 Stream API
+     */
+
+    /**
+     * java8 Date Time API
+     */
+    public static void testDate() {
+        LocalDate localDate = LocalDate.now();
+        System.out.println("Current Date=" + localDate);
+
+        LocalDate firstDay_2014 = LocalDate.of(2014, Month.JANUARY, 1);
+        System.out.println("Specific Date=" + firstDay_2014);
+
+
+        LocalDate todayKolkata = LocalDate.now(ZoneId.of("Asia/Kolkata"));
+        System.out.println("Current Date in IST=" + todayKolkata);
+
+        LocalDate dateFromBase = LocalDate.ofEpochDay(365);
+        System.out.println("365th day from base date= " + dateFromBase);
+
+        LocalDate hundredDay2014 = LocalDate.ofYearDay(2014, 100);
+        System.out.println("100th day of 2014=" + hundredDay2014);
+
+        System.out.println("=============================");
+        //Current Time
+        LocalTime time = LocalTime.now();
+        System.out.println("Current Time=" + time);
+
+        //Creating LocalTime by providing input arguments
+        LocalTime specificTime = LocalTime.of(12, 20, 25, 40);
+        System.out.println("Specific Time of Day=" + specificTime);
+
+        //Try creating time by providing invalid inputs
+        //LocalTime invalidTime = LocalTime.of(25,20);
+        //Exception in thread "main" java.time.DateTimeException:
+        //Invalid value for HourOfDay (valid values 0 - 23): 25
+
+        //Current date in "Asia/Kolkata", you can get it from ZoneId javadoc
+        LocalTime timeKolkata = LocalTime.now(ZoneId.of("Asia/Kolkata"));
+        System.out.println("Current Time in IST=" + timeKolkata);
+
+        //java.time.zone.ZoneRulesException: Unknown time-zone ID: IST
+        //LocalTime todayIST = LocalTime.now(ZoneId.of("IST"));
+
+        //Getting date from the base date i.e 01/01/1970
+        LocalTime specificSecondTime = LocalTime.ofSecondOfDay(10000);
+        System.out.println("10000th second time= " + specificSecondTime);
+
+        System.out.println("============================");
+
+        //Current Date
+        LocalDateTime today = LocalDateTime.now();
+        System.out.println("Current DateTime=" + today);
+
+        //Current Date using LocalDate and LocalTime
+        today = LocalDateTime.of(LocalDate.now(), LocalTime.now());
+        System.out.println("Current DateTime=" + today);
+
+        //Creating LocalDateTime by providing input arguments
+        LocalDateTime specificDate = LocalDateTime.of(2014, Month.JANUARY, 1, 10, 10, 30);
+        System.out.println("Specific Date=" + specificDate);
+
+        //Try creating date by providing invalid inputs
+        //LocalDateTime feb29_2014 = LocalDateTime.of(2014, Month.FEBRUARY, 28, 25,1,1);
+        //Exception in thread "main" java.time.DateTimeException:
+        //Invalid value for HourOfDay (valid values 0 - 23): 25
+
+        //Current date in "Asia/Kolkata", you can get it from ZoneId javadoc
+        LocalDateTime todayKolkata1 = LocalDateTime.now(ZoneId.of("Asia/Kolkata"));
+        System.out.println("Current Date in IST=" + todayKolkata1);
+
+        //java.time.zone.ZoneRulesException: Unknown time-zone ID: IST
+        //LocalDateTime todayIST = LocalDateTime.now(ZoneId.of("IST"));
+
+        //Getting date from the base date i.e 01/01/1970
+        LocalDateTime dateFromBase1 = LocalDateTime.ofEpochSecond(10000, 0, ZoneOffset.UTC);
+        System.out.println("10000th second time from 01/01/1970= " + dateFromBase1);
+
+        System.out.println("=======================");
+        //Current timestamp
+        Instant timestamp = Instant.now();
+        System.out.println("Current Timestamp = " + timestamp);
+
+        //Instant from timestamp
+        Instant specificTime1 = Instant.ofEpochMilli(timestamp.toEpochMilli());
+        System.out.println("Specific Time = " + specificTime1);
+
+        //Duration example
+        Duration thirtyDay = Duration.ofDays(30);
+        System.out.println(thirtyDay);
+    }
+
+    public static void testDate2() {
+        LocalDate today = LocalDate.now();
+
+        //Get the Year, check if it's leap year
+        System.out.println("Year " + today.getYear() + " is Leap Year? " + today.isLeapYear());
+
+        //Compare two LocalDate for before and after
+        System.out.println("Today is before 01/01/2015? " + today.isBefore(LocalDate.of(2015, 1, 1)));
+
+        //Create LocalDateTime from LocalDate
+        System.out.println("Current Time=" + today.atTime(LocalTime.now()));
+
+        //plus and minus operations
+        System.out.println("10 days after today will be " + today.plusDays(10));
+        System.out.println("3 weeks after today will be " + today.plusWeeks(3));
+        System.out.println("20 months after today will be " + today.plusMonths(20));
+
+        System.out.println("10 days before today will be " + today.minusDays(10));
+        System.out.println("3 weeks before today will be " + today.minusWeeks(3));
+        System.out.println("20 months before today will be " + today.minusMonths(20));
+
+        //Temporal adjusters for adjusting the dates
+        System.out.println("First date of this month= " + today.with(TemporalAdjusters.firstDayOfMonth()));
+        LocalDate lastDayOfYear = today.with(TemporalAdjusters.lastDayOfYear());
+        System.out.println("Last date of this year= " + lastDayOfYear);
+
+        Period period = today.until(lastDayOfYear);
+        System.out.println("Period Format= " + period);
+        System.out.println("Months remaining in the year= " + period.getMonths());
+    }
+
+    public static void testDate3() {
+        //Format examples
+        LocalDate date = LocalDate.now();
+        //default format
+        System.out.println("Default format of LocalDate=" + date);
+        //specific format
+        System.out.println(date.format(DateTimeFormatter.ofPattern("d::MMM::uuuu")));
+        System.out.println(date.format(DateTimeFormatter.BASIC_ISO_DATE));
+
+        LocalDateTime dateTime = LocalDateTime.now();
+        //default format
+        System.out.println("Default format of LocalDateTime=" + dateTime);
+        //specific format
+        System.out.println(dateTime.format(DateTimeFormatter.ofPattern("d::MMM::uuuu HH::mm::ss")));
+        System.out.println(dateTime.format(DateTimeFormatter.BASIC_ISO_DATE));
+
+        Instant timestamp = Instant.now();
+        //default format
+        System.out.println("Default format of Instant=" + timestamp);
+
+        //Parse examples
+        LocalDateTime dt = LocalDateTime.parse("27::Apr::2014 21::39::48",
+                DateTimeFormatter.ofPattern("dd::MMM::uuuu HH::mm::ss"));
+        System.out.println("Default format after parsing = " + dt);
+    }
+
+
+    /**
+     * 反射
+     */
+
+    /**
+     * 泛型
+     */
+
+
+    public static void main(String[] args) {
+        //System.out.println(12 * .12);
+        //testLambda();
+        //testLambda1();
+        //testLambda2();
+        //testLambda3();
+        //testLambda4();
+        //testLambda5();
+        //testLambda6();
+        //testLambda7();
+        //testLambda8();
+        //testLambda9();
+        //testLambda10();
+        //testDate();
+        //testDate2();
+        testDate3();
+    }
+
 }
